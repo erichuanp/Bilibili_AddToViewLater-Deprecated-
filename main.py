c4ea758cb0feb4908240baea59ccbc7b7471dfa9
Chuan Peng <pc951151824@gmail.com>
2022-07-18 18:16:11 -0700

    小修一下某个逻辑

--- main.py
+++ main.py
@@ -10,13 +10,14 @@ mode = open('Viewed.txt', 'r').readline(1)
 # endregion
 
 # region 询问并计算时间
+time_input = 3
 if mode != 'a':
     time_input = input('需要添加多少天以前的视频(默认为3)：')
     if not time_input.isdigit():
+        print('输入了非数字，默认解析前三天的视频。')
         time_input = 3
-else:
-    time_input = 3
 
+print('一般需要解析(2*天数+1)页视频...')
 end_time = int(time.time()) - int(time_input) * 86400  # 获取 time_input 天前的时间戳
 # endregion
 
